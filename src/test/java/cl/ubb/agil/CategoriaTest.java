26c3c3495a013f63c68c92bb57237c465a198bd8
elcasa123 <dcasanov@alumnos.ubiobio.cl>
2016-04-20 15:24:15 -0300

    test de Categoria Terminada

--- src/test/java/cl/ubb/agil/CategoriaTest.java
+++ src/test/java/cl/ubb/agil/CategoriaTest.java
@@ -11,9 +11,39 @@ public class CategoriaTest {
 	public void obtenerNombre(){
 		Categoria categoria=new Categoria("categ1","123");
 		String Resultado = categoria.getNombre();
-		assertThat(Resultado,is("categ1"));
+		assertThat(Resultado,is("categ1"));		
+	}
+	@Test
+	public void obtenerCodigoMayorA5(){
+		Categoria categoria=new Categoria("categ1","123456");
+		String Resultado = categoria.getCodigo();
+		assertThat(Resultado,is("12345"));
+	}
+	
+	@Test
+	public void obtenerCodigoMenorA5(){
+		Categoria categoria=new Categoria("categ1","123");
+		String Resultado = categoria.getCodigo();
+		assertThat(Resultado,is("123"));
+	}
+	
+	@Test
+	public void AsignarNombre(){
+		Categoria categoria = new Categoria("","");
+		categoria.setNombre("nombre");
+		String Resultado = categoria.getNombre();
+		assertThat(Resultado,is("nombre"));	
 		
 	}
+	@Test
+	public void AsignarCodigo(){
+		Categoria categoria = new Categoria("","");
+		categoria.setCodigo("codigo");
+		String Resultado = categoria.getCodigo();
+		assertThat(Resultado,is("codig"));	
+		
+	}
+	
 	
 	
 	
